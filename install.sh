1e3258d1cd60bacdd8a259f91d62b01c9b79498f
Pascal Immerzeel <pascal@immerzeel.net>
2014-01-10 14:18:57 +0000

    Setup script for symlinking and plugin installation.

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
-ln -s vimfiles ~/.vim
-ln -s vimfiles/vimrc ~/.vimrc
+ln -s ~/vimfiles ~/.vim
+ln -s ~/vimfiles/vimrc ~/.vimrc
+
+git clone https://github.com/gmarik/vundle.git ~/vimfiles/bundle/vundle
+vim -c "BundleUpdate"
 
 # TODO make it work on Windows.
-mkdir ~/Library/Vim
-mkdir ~/Library/Vim/{swap,undo,backup}
+# mkdir ~/Library/Vim
+# mkdir ~/Library/Vim/{swap,undo,backup}
