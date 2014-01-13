8fd4fb78a99858b048a86cd0913299fb3ae7beb5
Pascal Immerzeel <pascal@immerzeel.net>
2014-01-13 15:19:37 +0000

    Remove Mac specific settings for swap & backup.

--- install.sh
+++ install.sh
@@ -3,7 +3,3 @@ ln -s ~/vimfiles/vimrc ~/.vimrc
 
 git clone https://github.com/gmarik/vundle.git ~/vimfiles/bundle/vundle
 vim -c "BundleUpdate"
-
-# TODO make it work on Windows.
-# mkdir ~/Library/Vim
-# mkdir ~/Library/Vim/{swap,undo,backup}
